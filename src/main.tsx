e1b02bfc6c44d1d23e0ff03c1ceae13d05841a21
manuorlan1986-ui <manuorlan1986@gmail.com>
2026-09-24 22:42:19 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-
+import React, {useEffect, useMemo, useState} from 'react';
 import {createRoot} from 'react-dom/client';
 import {supabase} from './supabase';
 import {demoCaregivers, services} from './data';
